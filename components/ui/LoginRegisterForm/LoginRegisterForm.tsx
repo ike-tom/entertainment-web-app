57401b59b6c6821c1d1e83a3e192a9a29107a43c
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-11-12 22:51:59 +0100

    feat: add form validation for LoginRegisterForm component

--- components/ui/LoginRegisterForm/LoginRegisterForm.tsx
+++ components/ui/LoginRegisterForm/LoginRegisterForm.tsx
@@ -1,31 +1,48 @@
 import Link from 'next/link'
 import Image from 'next/image'
-
 import styles from './LoginRegisterForm.module.scss'
-import { FormEvent, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+interface IFormInput {
+  email: String
+  password: String
+  repeatPassword: String
+}
 
 const LoginRegisterForm = (props: { formType: string }) => {
   const { formType } = props
 
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [isSubmitting, setIsSubmitting] = useState(false);
-  // const [error, setError] = useState('');
+  const schema = yup.object({
+    email: yup.string().required(),
+    password: yup
+      .string()
+      .required('Password is required')
+      .min(4, 'Password length should be at least 4 characters')
+      .max(12, 'Password cannot exceed more than 12 characters'),
+    repeatPassword:
+      formType === 'Sign Up'
+        ? yup
+            .string()
+            .required('Confirm Password is required')
+            .min(4, 'Password length should be at least 4 characters')
+            .max(12, 'Password cannot exceed more than 12 characters')
+            .oneOf([yup.ref('password')], 'Passwords do not match')
+        : yup.string(),
+  })
 
-  // const onSubmit = async (e: FormEvent) => {
-  //     e.preventDefault();
-  //     setIsSubmitting(true);
-  //     setError('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  })
 
-  //     try {
-  //     await login(email, password);
-  //     // redirect to the home page
-  //     } catch (error) {
-  //     setError(error.message);
-  //     } finally {
-  //     setIsSubmitting(false);
-  //     }
-  // };
+  const onSubmit = (data: IFormInput) => {
+    alert(JSON.stringify(data))
+  }
 
   return (
     <main className={styles.main}>
@@ -38,28 +55,36 @@ const LoginRegisterForm = (props: { formType: string }) => {
           height={26}
         />
       </div>
-      <form className={styles.loginWrapper}>
+      <form className={styles.loginWrapper} onSubmit={handleSubmit(onSubmit)}>
         <h1 className={styles.header}>{formType}</h1>
 
         <input
+          {...register('email')}
           className={styles.emailInput}
           type="email"
           required
           placeholder="Email address"
         />
+        <p>{errors.email?.message}</p>
         <input
           className={styles.passwordInput}
           type="password"
           required
           placeholder="Password"
+          {...register('password')}
         />
+        <p>{errors.password?.message}</p>
         {formType === 'Sign Up' ? (
-          <input
-            className={styles.passwordInput}
-            type="password"
-            required
-            placeholder="Repeat password"
-          />
+          <>
+            <input
+              className={styles.passwordInput}
+              type="password"
+              required
+              placeholder="Repeat password"
+              {...register('repeatPassword')}
+            />
+            <p>{errors.repeatPassword?.message}</p>
+          </>
         ) : null}
         <button className={styles.loginButton}>
           {formType === 'Sign Up'
